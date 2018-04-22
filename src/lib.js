f8b93e2091ebf7f93df4d675c73b670bc5c3eff7
wmarttala <wmarttala123@gmail.com>
2018-04-22 00:25:46 -0500

    Update lib.js

--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,7 @@
-// TODO implement chooseRandom function and export it
 const chooseRandom = (array, numItems) => {
   // TODO check that array length is greater than 1
-  const randomIndices = Array(numItems) // TODO ensure numItems is within the inclusive range 1 to the length of the array
-    .fill()
+  const randomIndices = Array(numItems) // TODO ensure numItems is within the range 1 to the length of the array
+    .fill() // Don't change this line. Just fills the array we're about to reduce.
     .reduce((res, next) => {
       // reduce the array to contain numItems # of unique index values
     })
