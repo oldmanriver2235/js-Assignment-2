20f70380e14f6e71cca633ca98ef0d4008ea3107
wmarttala <wmarttala123@gmail.com>
2018-08-28 14:24:53 -0500

    Update lib.js

--- src/lib.js
+++ src/lib.js
@@ -1,10 +1,2 @@
 const chooseRandom = (array, numItems) => {
-  // TODO check that array length is greater than 1
-  const randomIndices = Array(numItems) // TODO ensure numItems is within the range 1 to the length of the array
-    .fill() // Don't change this line. Just fills the array we're about to reduce.
-    .reduce((res, next) => {
-      // reduce the array to contain numItems # of unique index values
-    })
-  // TODO return a new array filtering the passed in array for only the
-  // indices contained in the randomIndices array
 }
